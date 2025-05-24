2f601850922c62cbbc7acb102dae9008a13232df
WYHDDY <3456564906@qq.com>
2025-05-25 00:05:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,11 @@
 # See /LICENSE for more information.
 #
 
+sed -i 's/Os/O2/g' include/target.mk
+sed -i 's,-SNAPSHOT,,g' include/version.mk
+sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 sed -i 's/192.168.6.1/10.0.0.251/g' package/base-files/files/bin/config_generate
-
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
 sed -i '25s/cpu_freq=".*"/cpu_freq="$(mhz | awk -F '\''cpu_MHz='\'' '\''{printf("%.fMHz",$2)}'\'')";/' package/emortal/autocore/files/generic/cpuinfo
+sed -i '14s|MT7981_NEW_WOFW_DIR := .*|MT7981_NEW_WOFW_DIR := mt7981-fw-asus-ax57|' package/mtk/drivers/warp/Makefile
+sed -i '11s|MT7981_FW_DIR := .*|MT7981_FW_DIR := mt7981-asus-ax57|' package/mtk/drivers/mt_wifi/Makefile
